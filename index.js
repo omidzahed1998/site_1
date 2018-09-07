534ca08d5b62bed0f61f3a1cea0402bc8974cee5
omid zahed <pomidzahedp@gmail.com>
2018-09-07 12:49:35 +0430

    make body item

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//window.onload=function(){
+////var aa=document.getElementsByClassName("item_box_item_2_text");
+
+    //alert(aa[0].getAttribute("number"));
+    //aa[0].setAttribute("style","background:red;")
+
+
+
